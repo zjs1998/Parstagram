7d54cb77e885d7a5c8e760d88772f08b3d69de2e
Jingsheng Zhang <jxz5511@psu.edu>
2021-03-20 09:22:42 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Jingsheng Zhang on 3/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
